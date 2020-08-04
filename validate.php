cb639d3dc5497571d2dcf89801161fcde6be1872
givenmphiwe <65541697+givenmphiwe@users.noreply.github.com>
2020-08-05 00:08:22 +0200

    Update validate.php

--- validate.php
+++ validate.php
@@ -7,7 +7,14 @@ if(isset($_POST)['login'])){
     $pass  = $_POST['password'];
     $rem   = $_POST['remember'];
     if( $email == $myemail and $pass = $mypass){
-
+        if( isset($_POST['remember']) ){
+            setcookies('email', $email, time()+60*60*7);
+            setcookies('email', $email, time()+60*60*7);
+        }
+        session_start();
+        $_SESSION['email'] = $email;
+        header("location: profile.html");
+        
     }else{
         echo "ERROR Please use the provided login details<br> try again <a 
         href='login.php'";
